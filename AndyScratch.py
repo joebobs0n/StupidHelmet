c6ec9b14b7c8c851f673ee0bb2e5de98b25e0a76
Andy Monk <czech.monk90@gmail.com>
2020-04-09 15:31:23 -0600

    andy scratch push

--- AndyScratch.py
+++ AndyScratch.py
@@ -18,6 +18,14 @@ mat_r = right_params['mat']
 dist_l = left_params['dist']
 dist_r = right_params['dist']
 
+rectify = np.load('stereo_params/rectify_params.npz')
+R1 = rectify['R1']
+R2 = rectify['R2']
+P1 = rectify['P1']
+P2 = rectify['P2']
+
+l1, l2 = cv.initUndistortRectifyMap(mat_l, dist_l, R1, P1, (480, 640), cv.CV_32FC2)
+r1, r2 = cv.initUndistortRectifyMap(mat_r, dist_r, R2, P2, (480, 640), cv.CV_32FC2)
 
 while True:
     ret_l, frame_l = cam_l.read()
@@ -29,19 +37,24 @@ while True:
     if ret_l == False or ret_r == False:
         break
     
-    undistort_l = cv.undistort(gray_l, mat_l, dist_l)
-    undistort_r = cv.undistort(gray_r, mat_r, dist_r)
+    rectify_l = cv.remap(gray_l, l1, l2, cv.INTER_LINEAR)
+    rectify_r = cv.remap(gray_r, r1, r2, cv.INTER_LINEAR)
     
-    undistort_hstack = np.hstack([undistort_l, undistort_r])
+    undistort_hstack = np.hstack([rectify_l, rectify_r])
     cv.imshow('stereo frames', undistort_hstack)
     
-    disp = 16*4
-    block = 51
+    # disp = 128
+    # block = 21
     
-    stereo = cv.StereoBM_create(numDisparities=disp, blockSize=block)
-    disparity = stereo.compute(undistort_l, undistort_r)
+    stereo = cv.StereoBM_create()
+    stereo.setMinDisparity(4)
+    stereo.setNumDisparities(128)
+    stereo.setBlockSize(21)
+    stereo.setSpeckleRange(16)
+    stereo.setSpeckleWindowSize(45)
+    disparity = stereo.compute(rectify_l, rectify_r)
     
-    cv.imshow('preview', disparity)
+    cv.imshow('preview', disparity/1024)
     
     k = cv.waitKey(1) & 0xff
     if k == 27:
