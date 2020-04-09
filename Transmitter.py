a4831f8a02d35cc919624616935e304c0b7ce0fc
Michael Eyler <eyler94@gmail.com>
2020-04-09 16:21:45 -0600

    edit Transmitter.py

--- Transmitter.py
+++ Transmitter.py
@@ -9,10 +9,11 @@ port = 12345
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-num_rows = 50
+num_pages = 2
+num_rows = 25
 
 while True:
-    message = np.float32(np.random.randint(0,2,(num_rows,3))*255)
+    message = np.float32(np.random.randint(0,2,(num_pages,num_rows,3))*255)
     print("sending: \n", message)
     s.sendto(message, (host,port))
     sleep(1)
