1d6aa85b491bcebb0c23a9c168757e31925c93f9
Andy Monk <czech.monk90@gmail.com>
2020-04-12 17:16:51 -0600

    expanded functionality and commented

--- AndyScratch.py
+++ AndyScratch.py
@@ -1,89 +1,127 @@
-import numpy as np
-import cv2 as cv
-import imutils
-from matplotlib import pyplot as plt
-import os
-import sys
-import stupid_helmet_helpers as stpdhh
+# EXTRA NOTES:
+# This book is fantastic.... https://ebookcentral.proquest.com/lib/byu/reader.action?docID=4770094
 
-os.chdir(sys.path[0])
+import numpy as np  # used for calculations and array manipulations
+import cv2 as cv  # used for camera interface and robotic vision
+import imutils  # used for frame manipulations
+# from matplotlib import pyplot as plt
+import os  # used to change working directory
+import sys  # used to find working path
+import stupid_helmet_helpers as stpdhh  # custom supplemental functions/classes
 
+os.chdir(sys.path[0])  # change working directory to script path
+
+
+# function to close all things and exit script
+def quitScript():
+    cv.destroyAllWindows()
+    cam_l.release()
+    cam_r.release()
+    exit()
+
+
+# initialize left and right cameras (comment out video feed or camera feed to disable)
 cam_l = cv.VideoCapture('test_footage_left.avi')
 cam_r = cv.VideoCapture('test_footage_right.avi')
 # cam_l = cv.VideoCapture(3)
 # cam_r = cv.VideoCapture(2)
 
+# read in left camera intrinsic and distortion parameters
 left_params = np.load('stereo_params/left_params.npz')
-right_params = np.load('stereo_params/right_params.npz')
-
 mat_l = left_params['mat']
-mat_r = right_params['mat']
 dist_l = left_params['dist']
+
+# read in right camera intrinsic and distortion paramters
+right_params = np.load('stereo_params/right_params.npz')
+mat_r = right_params['mat']
 dist_r = right_params['dist']
 
+# read in stereo rectification paramters
 rectify = np.load('stereo_params/rectify_params.npz')
 R1 = rectify['R1']
 R2 = rectify['R2']
 P1 = rectify['P1']
 P2 = rectify['P2']
 
-l1, l2 = cv.initUndistortRectifyMap(mat_l, dist_l, R1, P1, (480, 640), cv.CV_32FC1)
-r1, r2 = cv.initUndistortRectifyMap(mat_r, dist_r, R2, P2, (480, 640), cv.CV_32FC1)
+# read in one frame from left and right cameras
+ret_l, frame_l = cam_l.read()
+ret_r, frame_r = cam_r.read()
+# check to see if a frame was received --> if not quit script
+if ret_l == False:
+    print('Cannot get frame from left camera. Exiting...')
+    quitScript()
+if ret_r == False:
+    print('Cannot get frame from right camera. Exiting...')
+    quitScript()
+# frame_l = imutils.rotate_bound(frame_l, 90 * 3)  # uncomment if using video feed
+# frame_r = imutils.rotate_bound(frame_r, 90 * 1)  # uncomment if using video feed
+# save shape of read frames
+shape_l = np.flip(np.shape(frame_l)[0:2])
+shape_r = np.flip(np.shape(frame_r)[0:2])
+# calculate left and right camera remapping
+l1, l2 = cv.initUndistortRectifyMap(
+    mat_l, dist_l, R1, P1, shape_l, cv.CV_32FC1)
+r1, r2 = cv.initUndistortRectifyMap(
+    mat_r, dist_r, R2, P2, shape_r, cv.CV_32FC1)
 
+# create stereo depth map object
 stereo = cv.StereoBM_create()
 
-# This book is fantastic.... https://ebookcentral.proquest.com/lib/byu/reader.action?docID=4770094
 
-stereo.setBlockSize(21)
-
-stereo.setMinDisparity(4)# # minimum disparity. Most use 0.
-stereo.setNumDisparities(112)#176 # the range of allowable disparities
+# set parameters
+stereo.setBlockSize(21)  # size of disparity block detection
+stereo.setMinDisparity(4)  # minimum disparity. Most use 0.
+# the range of allowable disparities -> multiples of 16
+stereo.setNumDisparities(112)
 
 # stereo.setPreFilterType(1) # 0 = prefilter to a normalized response, 1 is a x sobel filter
 # stereo.setPreFilterSize(21) # The block used for the prefilter
 # stereo.setPreFilterCap(32) # Saturation applied after prefiltering
-#
 # stereo.setTextureThreshold(1) # The amount of texture necessary for a disparity to be determined (otherwise => blank/black)
-#
 # stereo.setUniquenessRatio(1) # The amount of uniqueness required for two competing results for a "winner" to be declared
 
-stereo.setSpeckleWindowSize(10)#45 # Maximum size of a dettached blob to be considered as a "speckle"
-stereo.setSpeckleRange(45)#16 # Maximum allowable difference between pixels in a speckle. Otherwise flood-fill?
+# Maximum size of a dettached blob to be considered as a "speckle"
+stereo.setSpeckleWindowSize(10)
+# maximum allowable difference between disparities within speckle window
+stereo.setSpeckleRange(45)
 
 while True:
+    # read out left and right frames
     ret_l, frame_l = cam_l.read()
     ret_r, frame_r = cam_r.read()
-    # frame_l = imutils.rotate_bound(frame_l, 90 * 3)
-    # frame_r = imutils.rotate_bound(frame_r, 90 * 1)
+    # frame_l = imutils.rotate_bound(frame_l, 90 * 3)  # uncomment if using video feed
+    # frame_r = imutils.rotate_bound(frame_r, 90 * 1)  # uncomment if using video feed
 
-
+    # convert left and right frames to grayscale
     gray_l = cv.cvtColor(frame_l, cv.COLOR_BGR2GRAY)
-    # gray_l = np.flip(np.shape(gray_l))
     gray_r = cv.cvtColor(frame_r, cv.COLOR_BGR2GRAY)
 
+    # exit script if either left or right frame not read
     if ret_l == False or ret_r == False:
         break
 
+    # make left and right frames cononical
     rectify_l = cv.remap(gray_l, l1, l2, cv.INTER_LINEAR)
     rectify_r = cv.remap(gray_r, r1, r2, cv.INTER_LINEAR)
 
+    # stack rectified frames side by side for viewing
     undistort_hstack = np.hstack([rectify_l, rectify_r])
+    # show rectified frames
     cv.imshow('stereo frames', undistort_hstack)
 
+    # calculate depth map in terms of disparity
     disparity = stereo.compute(rectify_l, rectify_r)
-
-    # ret, disp_partitioned, disp_frame = stpdhh.parseDisparityMap(disparity, size=(2, 25, 3))
-    ret, disp_partitioned = stpdhh.parseDisparityMap(disparity, size=(2, 25, 3))
-    if ret == True:
-        print(disp_partitioned)
-
+    # display disparity map
     cv.imshow('preview', disparity/1024)
 
+    # calculate rgb representation to send
+    ret, disp_partitioned = stpdhh.parseDisparityMap(disparity)
+
+    # wait maximum 1 ms to read keystroke
     k = cv.waitKey(1) & 0xff
+    # if escape key read, exit script
     if k == 27:
         break
 
-cv.destroyAllWindows()
-cam_l.release()
-cam_r.release()
-exit()
+# close up and quit
+quitScript()
--- stupid_helmet_helpers.py
+++ stupid_helmet_helpers.py
@@ -1,46 +1,108 @@
 import numpy as np
 import socket
 
-# frame returned as y by x to keep with open cv dimension addressing
+
 def parseDisparityMap(frame, size=(2, 25, 3)):
+    # copy out frame data so we don't change it
     temp_frame = frame.copy()
+    # initialize return variables
     ret = True
+    parsed_frame = np.zeros(size)
 
-    parsed_frame = np.zeros(size))
-    # disp_frame = np.zeros(frame.shape)
+    # check to see if frame data provided
+    if frame is None:  # if not
+        # set return flag to unsuccessful
+        ret = False
+    else:
+        # get frame height (y) and width (x)
+        height = temp_frame.shape[0]
+        width = temp_frame.shape[1]
 
-    height = temp_frame.shape[0]
-    h_delta = height/size[0]
-    width = temp_frame.shape[1]
-    w_delta = width/size[1]
+        # calculate indices to split frame into desired rows and columns
+        x_indices = np.linspace(0, width, size[1]+1)
+        y_indices = np.linspace(0, height, size[0]+1)
 
-    x_indices = np.linspace(0, width, size[1]+1)
-    y_indices = np.linspace(0, height, size[0]+1)
+        # iterate through each partition
+        for y in range(size[0]):
+            for x in range(size[1]):
+                # calculate start and stop x and y indices
+                y0 = int(y_indices[y])
+                y1 = int(y_indices[y+1])
+                x0 = int(x_indices[x])
+                x1 = int(x_indices[x+1])
+                # pull off frame partition
+                partition = frame[y0:y1, x0:x1]
+                # here we can do whatever "statistical analysis" we want
+                # for now, i'm just averaging the data
+                partition_data = np.mean(partition)
+                # calculate rgb colors to represent disparity
+                rgb = [0, 0, 0]
+                if partition_data < 15:  # if nothing detected or very very close
+                    rgb = [0, 0, 128]  # show blue
+                elif partition_data > 1000:  # if something detected very far away
+                    rgb = [0, 0, 0]  # show black
+                else:  # if something detected within 1000 disparity
+                    # calculate color mixed from red to green
+                    rgb = [((1000 - partition_data) * 0.128),
+                           partition_data * 0.128, 0]
+                # save rgb value in parsed frame data
+                parsed_frame[y, x] = rgb
 
-    for y in range(size[0]):
-        for x in range(size[1]):
-            y0 = int(y_indices[y])
-            y1 = int(y_indices[y+1])
-            x0 = int(x_indices[x])
-            x1 = int(x_indices[x+1])
-            partition = frame[y0:y1, x0:x1]
-            # here we can do whatever "statistical analysis" we want
-            # for now, i'm just averaging the data
-            partition_data = np.mean(partition)
-            rgb = 
-            parsed_frame[y, x] = partition_data
+    # returned frame is [y, x, z] where z is color
+    return ret, parsed_frame
 
-    return ret, parsed_frame#, disp_frame
 
 class Transmitter():
-    def __init__(self, IpAddr='127.0.0.1',UdpPort=12345):
+    # custom class constructor
+    def __init__(self, IpAddr='127.0.0.1', UdpPort=12345):
+        # intialize socket
         self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.IP = IpAddr
-        self.port = UdpPort
+        # set working IP address
+        self.SetIP(IpAddr)
+        # set working port
+        self.SetPort(UdpPort)
+        # set transmission data dimensions
+        self.SetTransmitDims()
 
+    # method to send data
     def SendData(self, array=None):
-        if array not None:
+        if array is not None:  # if data passed into method
+            # transmit desired data
             message = np.float32(array)
-        else:
-            message = np.float32(np.random.randint(0,2,(num_pages,num_rows,3))*255)
+        else:  # if no data passed into method
+            # transmit random data
+            message = np.float32(np.random.randint(
+                0, 2, (self.pages, self.rows, 3)) * 255)
         self.s.sendto(message, (self.IP, self.port))
+
+    # method to set working port number
+    def SetPort(self, newPort=12345):
+        # set port to default '12345' or passed integer
+        self.port = int(newPort)
+
+    # method to see current port number
+    def GetPort(self):
+        # return programmed port number
+        return self.port
+
+    # method to set working ip address
+    def SetIP(self, newIP='127.0.0.1'):
+        # set IP to default '127.0.0.1' or passed address
+        self.IP = newIP
+
+    # method to see current IP addres
+    def GetIP(self):
+        # return programmed IP address
+        return self.IP
+
+    # method to set transmit data dimensions
+    def SetTransmitDims(self, num_pages=25, num_rows=2):
+        # set pages (columns) to default '25' or passed integer
+        self.pages = int(num_pages)
+        # set rows to default '2' or passed integer
+        self.rows = int(num_rows)
+
+    # method to see current transmit data dimensions
+    def GetTransmitDims(self):
+        # return programmed transmit dimensions data (with included '3' for channel)
+        return (self.pages, self.rows, 3)
